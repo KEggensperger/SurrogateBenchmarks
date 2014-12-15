132e68b0efa70f35f3ae10e233053f1e645f9660
Katharina Eggensperger <eggenspk@informatik.uni-freiburg.de>
2014-12-15 10:33:53 +0100

    landscape.io

--- Surrogates/RegressionModels/RandomForest.py
+++ Surrogates/RegressionModels/RandomForest.py
@@ -36,7 +36,8 @@ from Surrogates.DataExtraction.data_util import read_csv
 class RandomForest(ScikitBaseClass.ScikitBaseClass):
 
     def __init__(self, sp, encode, rng, **kwargs):
-        ScikitBaseClass.ScikitBaseClass.__init__(self, sp=sp, encode=encode, rng=rng, **kwargs)
+        ScikitBaseClass.ScikitBaseClass.__init__(self, sp=sp, encode=encode,
+                                                 rng=rng, **kwargs)
         self._name = "RandomForest " + str(encode)
 
     def _random_search(self, random_iter, x, y):
@@ -54,7 +55,9 @@ class RandomForest(ScikitBaseClass.ScikitBaseClass):
             param_list = [{"n_estimators": n_estimators,
                            "min_samples_split": min_samples_split,
                            "max_features": max_features}, ]
-            param_list.extend(list(ParameterSampler(param_dist, n_iter=random_iter-1, random_state=self._rng)))
+            param_list.extend(list(ParameterSampler(param_dist,
+                                                    n_iter=random_iter-1,
+                                                    random_state=self._rng)))
             for idx, d in enumerate(param_list):
                 rf = RandomForestRegressor(n_estimators=int(d["n_estimators"]),
                                            criterion='mse',
@@ -67,9 +70,12 @@ class RandomForest(ScikitBaseClass.ScikitBaseClass):
                                            oob_score=False,
                                            n_jobs=1,
                                            random_state=self._rng,
-                                           verbose=0, min_density=None, compute_importances=None)
+                                           verbose=0, min_density=None,
+                                           compute_importances=None)
 
-                train_x, test_x, train_y, test_y = train_test_split(x, y, test_size=0.5, random_state=self._rng)
+                train_x, test_x, train_y, test_y = \
+                    train_test_split(x, y, test_size=0.5,
+                                     random_state=self._rng)
                 rf.fit(train_x, train_y)
                 sc = rf.score(test_x, test_y)
                 # Tiny output
@@ -84,7 +90,10 @@ class RandomForest(ScikitBaseClass.ScikitBaseClass):
                     max_features = d["max_features"]
                 sys.stdout.write(m)
                 sys.stdout.flush()
-            sys.stdout.write("Using n_estimators: %d, min_samples_split: %d, and max_features: %f\n" % (n_estimators, min_samples_split, max_features))
+            sys.stdout.write("Using n_estimators: %d, min_samples_split: %d, "
+                             "and max_features: %f\n" % (n_estimators,
+                                                         min_samples_split,
+                                                         max_features))
         return n_estimators, min_samples_split, max_features
 
     def train(self, x, y, param_names, random_search=100, **kwargs):
@@ -101,7 +110,8 @@ class RandomForest(ScikitBaseClass.ScikitBaseClass):
             print "Encode: ", self._encode
 
         # Do a random search
-        n_estimators, min_samples_split, max_features = self._random_search(random_iter=100, x=scaled_x, y=y)
+        n_estimators, min_samples_split, max_features = \
+            self._random_search(random_iter=random_search, x=scaled_x, y=y)
 
         rf = RandomForestRegressor(n_estimators=n_estimators,
                                    criterion='mse',
@@ -114,79 +124,11 @@ class RandomForest(ScikitBaseClass.ScikitBaseClass):
                                    oob_score=False,
                                    n_jobs=1,
                                    random_state=self._rng,
-                                   verbose=0, min_density=None, compute_importances=None)
+                                   verbose=0, min_density=None,
+                                   compute_importances=None)
         rf.fit(scaled_x, y)
         self._model = rf
 
         duration = time.time() - start
         self._training_finished = True
         return duration
-
-
-def test():
-    from sklearn.metrics import mean_squared_error
-    import Surrogates.DataExtraction.pcs_parser as pcs_parser
-    sp = pcs_parser.read(file("/home/eggenspk/Surrogates/Data_extraction/Experiments2014/hpnnet/smac_2_06_01-dev/nips2011.pcs"))
-    # Read data from csv
-    header, data = read_csv("/home/eggenspk/Surrogates/Data_extraction/hpnnet_nocv_convex_all/hpnnet_nocv_convex_all_fastrf_results.csv",
-                            has_header=True, num_header_rows=3)
-    para_header = header[0]
-    type_header = header[1]
-    cond_header = header[2]
-    checkpoint = hash(numpy.array_repr(data))
-    assert checkpoint == 246450380584980815
-
-    model = RandomForest(sp=sp, encode=False, debug=True, rng=1)
-    x_train_data = data[:1000, :-2]
-    y_train_data = data[:1000, -1]
-    x_test_data = data[1000:, :-2]
-    y_test_data = data[1000:, -1]
-
-    model.train(x=x_train_data, y=y_train_data, param_names=para_header)
-    assert model._model.get_params()['n_estimators'] == 70
-    assert model._model.get_params()['min_samples_split'] == 2
-    assert model._model.get_params()['max_features'] == 0.4
-
-    str_data = x_train_data[1, :]
-    str_data = list(str_data)
-    y1 = model.predict(x=str_data)
-    print "Is: %100.70f, Should: %f" % (y1, y_train_data[1])
-    str_data[0] = str(str_data[0])
-    y2 = model.predict(x=str_data)
-    print "Is: %100.70f, Should: %f" % (y2, y_train_data[1])
-    assert y1 == y2
-
-    y = model.predict(x=x_train_data[1, :])
-    print "Is: %100.70f, Should: %f" % (y, y_train_data[1])
-    assert y[0] == 0.438562527380952638100808371746097691357135772705078125
-
-    print "Predict whole data"
-    y_whole = model.predict(x=x_test_data)
-    mse = mean_squared_error(y_true=y_test_data, y_pred=y_whole)
-    print "MSE: %100.70f" % mse
-    assert mse == 0.0027243939040595683935552795418288951623253524303436279296875
-
-    print "Soweit so gut"
-
-    # Try the same with encoded features
-    model = RandomForest(sp=sp, encode=True, debug=True, rng=1)
-    #print data[:10, :-2]
-    model.train(x=x_train_data, y=y_train_data, param_names=para_header)
-
-    y = model.predict(x=x_train_data[1, :])
-    print "Is: %100.70f, Should: %f" % (y, y_train_data[1])
-    assert y[0] == 0.435386310714285962308878197291051037609577178955078125
-
-    print "Predict whole data"
-    y_whole = model.predict(x=x_test_data)
-    mse = mean_squared_error(y_true=y_test_data, y_pred=y_whole)
-    print "MSE: %100.70f" % mse
-    assert mse == 0.0025204000192945203058514369587328474153764545917510986328125
-
-    assert hash(numpy.array_repr(data)) == checkpoint
-
-if __name__ == "__main__":
-    outer_start = time.time()
-    test()
-    dur = time.time() - outer_start
-    print "TESTING TOOK: %f sec" % dur
